8097e5257e46be68fba655948cb4705fddf7a230
jfantin75 <jose_fantin@msn.com>
2025-05-06 10:38:21 -0700

    Update constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -1,31 +1,34 @@
 // utils/constants.js
 
-export const ductDiameters = [
-  '3', '4', '5', '6', '7', '8', '9', '10'
-];
+export const ductDiameters = [4, 5, 6, 7, 8];
 
-export const materialTypes = [
-  { label: 'Metal', value: 'metal', frictionFactor: 0.06 },
-  { label: 'PVC', value: 'pvc', frictionFactor: 0.07 }
-];
+export const materialTypes = {
+  Metal: {
+    straight: 0.015,
+    flex: 0.2,
+  },
+  PVC: {
+    straight: 0.05,
+    flex: 0.3,
+  },
+};
 
-export const componentOptions = [
-  { label: '45° Elbow', value: 'elbow_45', baseSP: 0.3 },
-  { label: '90° Elbow', value: 'elbow_90', baseSP: 0.75 },
-  { label: 'Wye', value: 'wye', baseSP: 0.6 },
-  { label: 'Tee', value: 'tee', baseSP: 0.9 },
-  { label: 'Blast Gate', value: 'blast_gate', baseSP: 0.15 },
-  { label: 'Reducer', value: 'reducer', baseSP: 0.2 }
-];
+export const componentLossValues = {
+  ELBOW45: 0.1,
+  ELBOW90: 0.25,
+  WYE: 0.3,
+  TEE: 0.9,
+  BLASTGATE: 0.03,
+};
 
-export const cycloneOptions = [
-  { label: 'None', value: 'none', sp: 0 },
-  { label: 'Basic Cyclone', value: 'basic', sp: 2.0 },
-  { label: 'High Efficiency Cyclone', value: 'high_efficiency', sp: 1.2 }
-];
+export const cycloneTypes = {
+  None: 0,
+  'Basic Cyclone': 0.5,
+  'High Efficiency Cyclone': 0.35,
+};
 
-export const filterOptions = [
-  { label: 'None', value: 'none', sp: 0 },
-  { label: 'Standard Filter', value: 'standard', sp: 1.5 },
-  { label: 'HEPA Filter', value: 'hepa', sp: 2.2 }
-];
+export const filterTypes = {
+  None: 0,
+  'Standard Cartridge Filter': 0.7,
+  'HEPA Filter': 1.2,
+};

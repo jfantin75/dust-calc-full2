821336f500f7b241939f744e6fcba109a87eebfb
jfantin75 <jose_fantin@msn.com>
2025-05-06 10:17:04 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-import dynamic from 'next/dynamic';
-const DustCollectionCalculator = dynamic(() => import('../components/DustCollectionCalculator'), { ssr: false });
+// File: pages/index.js
+import Head from 'next/head';
+import DustCollectionCalculator from '../components/DustCollectionCalculator';
 
 export default function Home() {
-  return <DustCollectionCalculator />;
+  return (
+    <div>
+      <Head>
+        <title>Dust Collection Calculator</title>
+        <meta name="description" content="Calculate static pressure and CFM for your dust collection system" />
+      </Head>
+      <main>
+        <DustCollectionCalculator />
+      </main>
+    </div>
+  );
 }
